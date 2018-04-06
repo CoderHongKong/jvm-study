d4cdaa9a1b35d7fb82ff1973eacdcf68215f2c01
hekai <hekai0118@gmail.com>
2018-04-06 22:01:02 +0800

    类的命名空间

--- src/main/java/com/blueberry/jvm/classloader/MyTest21.java
+++ src/main/java/com/blueberry/jvm/classloader/MyTest21.java
@@ -7,10 +7,14 @@ import java.lang.reflect.Method;
  * @create 2018-03-31 上午9:38
  */
 
-public class MyTest20 {
+public class MyTest21 {
     public static void main(String[] args) throws Exception{
         MyTest16 loader1 = new MyTest16("loader1");
         MyTest16 loader2 = new MyTest16("loader2");
+
+        loader1.setPath("/Users/hekai/Desktop/");
+        loader2.setPath("/Users/hekai/Desktop/");
+
         Class<?> clazz = loader1.loadClass("com.blueberry.jvm.classloader.MyPerson");
         Class<?> clazz2 = loader1.loadClass("com.blueberry.jvm.classloader.MyPerson");
 
