7ac131815e9a0bcfcfc7dce568455fb495b0083f
hekai <hekai0118@gmail.com>
2018-06-03 11:37:13 +0800

    线程上下文类加载器mysql驱动加载

--- src/main/java/com/blueberry/jvm/classloader/MyTest26.java
+++ src/main/java/com/blueberry/jvm/classloader/MyTest26.java
@@ -1,5 +1,9 @@
 package com.blueberry.jvm.classloader;
 
+import java.sql.Driver;
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
 /**
  * @author Alman
  * @date 2018-05-13 上午10:05
@@ -7,4 +11,19 @@ package com.blueberry.jvm.classloader;
 
 public class MyTest26 {
 
+    public static void main(String[] args) {
+
+        Thread.currentThread().setContextClassLoader(MyTest26.class.getClassLoader().getParent());
+
+
+        ServiceLoader<Driver> loader = ServiceLoader.load(Driver.class);
+        Iterator<Driver> iterator = loader.iterator();
+        while (iterator.hasNext()) {
+            Driver driver = iterator.next();
+            System.out.println("driver: " + driver.getClass() + ",loader: " + driver.getClass().getClassLoader());
+        }
+
+        System.out.println("当前线程上下文类加载器：" + Thread.currentThread().getContextClassLoader());
+        System.out.println("ServiceLoader的类加载器：" + ServiceLoader.class.getClassLoader());
+    }
 }
